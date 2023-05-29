11fcfcb9717ca36a9ac08ece60723ec8d22a98f0
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-05-29 18:43:55 +0200

    feat: prepare files for deploy

--- Build-DAO-for-NFT-holders/scripts/deploy.js
+++ Build-DAO-for-NFT-holders/scripts/deploy.js
@@ -5,22 +5,33 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+require("dotenv").config();
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = process.env.CRYPTODEVS_NFT_CONTRACT_ADDRESS
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  // Deploy the FakeNFTMarketplace contract first
+  const FakeNFTMarketplace = await ethers.getContractFactory(
+    "FakeNFTMarketplace"
   );
+  const fakeNftMarketplace = await FakeNFTMarketplace.deploy();
+  await fakeNftMarketplace.deployed();
+
+  console.log("FakeNFTMarketplace deployed to: ", fakeNftMarketplace.address);
+
+  // Now deploy the CryptoDevsDAO contract
+  const CryptoDevsDAO = await ethers.getContractFactory("CryptoDevsDAO");
+  const cryptoDevsDAO = await CryptoDevsDAO.deploy(
+    fakeNftMarketplace.address,
+    CRYPTODEVS_NFT_CONTRACT_ADDRESS,
+    {
+      // This assumes your metamask account has at least 1 ETH in its account
+      // Change this value as you want
+      value: ethers.utils.parseEther("1"),
+    }
+  );
+  await cryptoDevsDAO.deployed();
+
+  console.log("CryptoDevsDAO deployed to: ", cryptoDevsDAO.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
