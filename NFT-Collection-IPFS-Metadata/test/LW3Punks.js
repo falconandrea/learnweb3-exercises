68a3e6af6080f79923924c480e873efffb3fb31d
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-06-12 20:16:27 +0200

    feat: fix bug new hardhat version

--- NFT-Collection-IPFS-Metadata/test/LW3Punks.js
+++ NFT-Collection-IPFS-Metadata/test/LW3Punks.js
@@ -1,7 +1,7 @@
 const {
   time,
   loadFixture,
-} = require("@nomicfoundation/hardhat-toolbox/network-helpers")
+} = require("@nomicfoundation/hardhat-network-helpers")
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs")
 const { expect } = require("chai")
 require('dotenv').config()
@@ -40,5 +40,63 @@ describe("LW3Punks", function () {
       expect(await deployedLW3PunksContract.owner()).to.equal(owner.address)
     })
     */
+  })
+
+  describe("Mint", function () {
+    /*
+    it('Should block mint when contract is paused', async function () {
+      const { contract } = await loadFixture(deployFixture)
+
+      expect(lW3Punks.address).to.be.a('string')
+    })
+
+    it("Should block mint if eth amount is wrong", async function () {
+      const { lW3Punks, owner } = await loadFixture(deployFixture)
+
+      expect(await lW3Punks.owner()).to.equal(owner.address)
+    })
+
+    it("Should block mint if nft amount is reached", async function () {
+      const { deployedLW3PunksContract } = await loadFixture(deployFixture)
+
+      const maxTokenIds = await deployedLW3PunksContract.maxTokenIds()
+      console.log(maxTokenIds)
+
+      //for(let i = 0; i < )
+
+      // expect(await lW3Punks.owner()).to.equal(owner.address)
+    })
+
+    it("Should give error if tokenURI receive wrong token id", async function () {
+      const { deployedLW3PunksContract } = await loadFixture(deployFixture)
+
+      await expect(deployedLW3PunksContract.tokenURI(12)).to.be.revertedWith(
+        'ERC721Metadata: URI query for nonexistent token'
+      )
+    })
+*/
+  })
+
+  describe("Withdraw", function () {
+    /*
+    it("Should revert with the right error if called from another account", async function () {
+      const { deployedLW3PunksContract, otherAccount } = await loadFixture(
+        deployFixture
+      )
+
+      // We use deployedLW3PunksContract.connect() to send a transaction from another account
+      await expect(deployedLW3PunksContract.connect(otherAccount).withdraw()).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      )
+    })
+
+    it("Shouldn't fail if the owner calls it", async function () {
+      const { deployedLW3PunksContract, owner } = await loadFixture(
+        deployFixture
+      )
+
+      await expect(deployedLW3PunksContract.connect(owner).withdraw()).not.to.be.reverted
+    })
+*/
   })
 })
