c7cf69d6543556b3ff3402ddf6aee3f698871542
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-06-17 17:16:33 +0200

    feat: complete contract and deploy script + fix some bugs in tutorial code

--- Build-Lottery-Game-with-Chainlink/hardhat.config.js
+++ Build-Lottery-Game-with-Chainlink/hardhat.config.js
@@ -5,10 +5,14 @@ require('dotenv').config()
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    // Change if you need
     mumbai: {
       url: process.env.URL_PROVIDER,
-      accounts: [process.env.PRIVATE_KEY]
-    }
-  }
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_KEY,
+    },
+  },
 };
--- Build-Lottery-Game-with-Chainlink/scripts/deploy.js
+++ Build-Lottery-Game-with-Chainlink/scripts/deploy.js
@@ -1,24 +1,42 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config();
+const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
 
 async function main() {
-  const simpleNFT = await hre.ethers.deployContract("SimpleNFT");
-
-  await simpleNFT.waitForDeployment();
-
-  console.log(
-    `SimpleNFT deployed to ${simpleNFT.target}`
+  /*
+  DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+  so randomWinnerGame here is a factory for instances of our RandomWinnerGame contract.
+  */
+  // deploy the contract
+  const randomWinnerGame = await hre.ethers.deployContract(
+    "RandomWinnerGame",
+    [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE]
   );
+
+  await randomWinnerGame.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("Verify Contract Address:", randomWinnerGame.target);
+
+  console.log("Sleeping.....");
+  // Wait for etherscan to notice that the contract has been deployed
+  await sleep(30000);
+
+  // Verify the contract after deploying
+  await hre.run("verify:verify", {
+    address: randomWinnerGame.target,
+    constructorArguments: [VRF_COORDINATOR, LINK_TOKEN, KEY_HASH, FEE],
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
