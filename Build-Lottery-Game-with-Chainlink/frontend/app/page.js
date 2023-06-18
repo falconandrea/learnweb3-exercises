c44c0538334b46d5f734fbb0d2da5a165d87443b
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-06-18 17:17:42 +0200

    feat: copy paste code from tutorial

--- Build-Lottery-Game-with-Chainlink/frontend/app/page.js
+++ Build-Lottery-Game-with-Chainlink/frontend/app/page.js
@@ -1,27 +1,38 @@
-"use client";
-
-import { Contract, providers, utils } from "ethers";
+import { BigNumber, Contract, ethers, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import styles from './page.module.css'
-
-const NFT_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
-import contract from "artifactsContracts/SimpleNFT.sol/SimpleNFT"
-const abi = contract.abi
-const NETWORK_NAME = process.env.NEXT_PUBLIC_NETWORK_NAME
-const NETWORK_ID = process.env.NEXT_PUBLIC_NETWORK_ID
+import { abi, RANDOM_GAME_NFT_CONTRACT_ADDRESS } from "../constants";
+import { FETCH_CREATED_GAME } from "../queries";
+import styles from "../styles/Home.module.css";
+import { subgraphQuery } from "../utils";
 
 export default function Home() {
+  const zero = BigNumber.from("0");
   // walletConnected keep track of whether the user's wallet is connected or not
   const [walletConnected, setWalletConnected] = useState(false);
   // loading is set to true when we are waiting for a transaction to get mined
   const [loading, setLoading] = useState(false);
+  // boolean to keep track of whether the current connected account is owner or not
+  const [isOwner, setIsOwner] = useState(false);
+  // entryFee is the ether required to enter a game
+  const [entryFee, setEntryFee] = useState(zero);
+  // maxPlayers is the max number of players that can play the game
+  const [maxPlayers, setMaxPlayers] = useState(0);
+  // Checks if a game started or not
+  const [gameStarted, setGameStarted] = useState(false);
+  // Players that joined the game
+  const [players, setPlayers] = useState([]);
+  // Winner of the game
+  const [winner, setWinner] = useState();
+  // Keep a track of all the logs for a given game
+  const [logs, setLogs] = useState([]);
   // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
   const web3ModalRef = useRef();
-  // for messages
-  const [errorMessage, setErrorMessage] = useState('')
-  const [successMessage, setSuccessMessage] = useState('')
+
+  // This is used to force react to re render the page when we want to
+  // in our case we will use force update to show new logs
+  const forceUpdate = React.useReducer(() => ({}), {})[1];
 
   /*
     connectWallet: Connects the MetaMask wallet
@@ -40,6 +51,13 @@ export default function Home() {
   /**
    * Returns a Provider or Signer object representing the Ethereum RPC with or without the
    * signing capabilities of metamask attached
+   *
+   * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+   *
+   * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+   * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+   * request signatures from the user using Signer functions.
+   *
    * @param {*} needSigner - True if you need the signer, default false otherwise
    */
   const getProviderOrSigner = async (needSigner = false) => {
@@ -48,11 +66,11 @@ export default function Home() {
     const provider = await web3ModalRef.current.connect();
     const web3Provider = new providers.Web3Provider(provider);
 
-    // If user is not connected to the network, let them know and throw an error
+    // If user is not connected to the Mumbai network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (parseInt(chainId) !== parseInt(NETWORK_ID)) {
-      setErrorMessage(`Change the network to ${NETWORK_NAME}`);
-      throw new Error(`Change the network to ${NETWORK_NAME}`);
+    if (chainId !== 80001) {
+      window.alert("Change the network to Mumbai");
+      throw new Error("Change network to Mumbai");
     }
 
     if (needSigner) {
@@ -60,6 +78,142 @@ export default function Home() {
       return signer;
     }
     return web3Provider;
+  };
+
+  /**
+   * startGame: Is called by the owner to start the game
+   */
+  const startGame = async () => {
+    try {
+      // Get the signer from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const signer = await getProviderOrSigner(true);
+      // We connect to the Contract using a signer because we want the owner to
+      // sign the transaction
+      const randomGameNFTContract = new Contract(
+        RANDOM_GAME_NFT_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      setLoading(true);
+      // call the startGame function from the contract
+      const tx = await randomGameNFTContract.startGame(maxPlayers, entryFee);
+      await tx.wait();
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+      setLoading(false);
+    }
+  };
+
+  /**
+   * startGame: Is called by a player to join the game
+   */
+  const joinGame = async () => {
+    try {
+      // Get the signer from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const signer = await getProviderOrSigner(true);
+      // We connect to the Contract using a signer because we want the owner to
+      // sign the transaction
+      const randomGameNFTContract = new Contract(
+        RANDOM_GAME_NFT_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      setLoading(true);
+      // call the startGame function from the contract
+      const tx = await randomGameNFTContract.joinGame({
+        value: entryFee,
+      });
+      await tx.wait();
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  /**
+   * checkIfGameStarted checks if the game has started or not and intializes the logs
+   * for the game
+   */
+  const checkIfGameStarted = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const randomGameNFTContract = new Contract(
+        RANDOM_GAME_NFT_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      // read the gameStarted boolean from the contract
+      const _gameStarted = await randomGameNFTContract.gameStarted();
+
+      const _gameArray = await subgraphQuery(FETCH_CREATED_GAME());
+      const _game = _gameArray.games[0];
+      let _logs = [];
+      // Initialize the logs array and query the graph for current gameID
+      if (_gameStarted) {
+        _logs = [`Game has started with ID: ${_game.id}`];
+        if (_game.players && _game.players.length > 0) {
+          _logs.push(
+            `${_game.players.length} / ${_game.maxPlayers} already joined 👀 `
+          );
+          _game.players.forEach((player) => {
+            _logs.push(`${player} joined 🏃‍♂️`);
+          });
+        }
+        setEntryFee(BigNumber.from(_game.entryFee));
+        setMaxPlayers(_game.maxPlayers);
+      } else if (!gameStarted && _game.winner) {
+        _logs = [
+          `Last game has ended with ID: ${_game.id}`,
+          `Winner is: ${_game.winner} 🎉 `,
+          `Waiting for host to start new game....`,
+        ];
+
+        setWinner(_game.winner);
+      }
+      setLogs(_logs);
+      setPlayers(_game.players);
+      setGameStarted(_gameStarted);
+      forceUpdate();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /**
+   * getOwner: calls the contract to retrieve the owner
+   */
+  const getOwner = async () => {
+    try {
+      // Get the provider from web3Modal, which in our case is MetaMask
+      // No need for the Signer here, as we are only reading state from the blockchain
+      const provider = await getProviderOrSigner();
+      // We connect to the Contract using a Provider, so we will only
+      // have read-only access to the Contract
+      const randomGameNFTContract = new Contract(
+        RANDOM_GAME_NFT_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      // call the owner function from the contract
+      const _owner = await randomGameNFTContract.owner();
+      // We will get the signer now to extract the address of the currently connected MetaMask account
+      const signer = await getProviderOrSigner(true);
+      // Get the address associated to the signer which is connected to  MetaMask
+      const address = await signer.getAddress();
+      if (address.toLowerCase() === _owner.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   // useEffects are used to react to changes in state of the website
@@ -71,45 +225,22 @@ export default function Home() {
       // Assign the Web3Modal class to the reference object by setting it's `current` value
       // The `current` value is persisted throughout as long as this page is open
       web3ModalRef.current = new Web3Modal({
-        network: NETWORK_NAME,
+        network: "mumbai",
         providerOptions: {},
         disableInjectedProvider: false,
       });
-
-      connectWallet()
+      connectWallet();
+      getOwner();
+      checkIfGameStarted();
+      setInterval(() => {
+        checkIfGameStarted();
+      }, 2000);
     }
   }, [walletConnected]);
 
-  /**
-   * publicMint: Mint an NFT after the presale
-   */
-  const publicMint = async () => {
-    try {
-      // Reset messages
-      setErrorMessage('')
-      setSuccessMessage('')
-      // We need a Signer here since this is a 'write' transaction.
-      const signer = await getProviderOrSigner(true)
-      // Create a new instance of the Contract with a Signer, which allows
-      // update methods
-      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer)
-      // call the mint from the contract to mint the NFT
-      const tx = await nftContract.mint({
-        // value signifies the cost of one NFT which is "0.01" eth.
-        // We are parsing `0.01` string to ether using the utils library from ethers.js
-        value: utils.parseEther('0.01')
-      })
-      setLoading(true)
-      // wait for the transaction to get mined
-      await tx.wait()
-      setLoading(false)
-      setSuccessMessage('You successfully minted your NFT!')
-    } catch (err) {
-      console.error(err)
-      setErrorMessage('Error during public mint')
-    }
-  }
-
+  /*
+    renderButton: Returns a button based on the state of the dapp
+  */
   const renderButton = () => {
     // If wallet is not connected, return a button which allows them to connect their wallet
     if (!walletConnected) {
@@ -120,48 +251,90 @@ export default function Home() {
       );
     }
 
-    return (
-      <button className={styles.button} onClick={publicMint}>
-        Public Mint 🚀
-      </button>
-    );
+    // If we are currently waiting for something, return a loading button
+    if (loading) {
+      return <button className={styles.button}>Loading...</button>;
+    }
+    // Render when the game has started
+    if (gameStarted) {
+      if (players.length === maxPlayers) {
+        return (
+          <button className={styles.button} disabled>
+            Choosing winner...
+          </button>
+        );
+      }
+      return (
+        <div>
+          <button className={styles.button} onClick={joinGame}>
+            Join Game 🚀
+          </button>
+        </div>
+      );
+    }
+    // Start the game
+    if (isOwner && !gameStarted) {
+      return (
+        <div>
+          <input
+            type="number"
+            className={styles.input}
+            onChange={(e) => {
+              // The user will enter the value in ether, we will need to convert
+              // it to WEI using parseEther
+              setEntryFee(
+                e.target.value >= 0
+                  ? utils.parseEther(e.target.value.toString())
+                  : zero
+              );
+            }}
+            placeholder="Entry Fee (ETH)"
+          />
+          <input
+            type="number"
+            className={styles.input}
+            onChange={(e) => {
+              // The user will enter the value for maximum players that can join the game
+              setMaxPlayers(e.target.value ?? 0);
+            }}
+            placeholder="Max players"
+          />
+          <button className={styles.button} onClick={startGame}>
+            Start Game 🚀
+          </button>
+        </div>
+      );
+    }
   };
-
-  const renderMessages = () => {
-    if(successMessage) {
-      return (
-        <p className={styles.successMessage}>
-          {successMessage}
-        </p>
-      )
-    }
-    if(errorMessage) {
-      return (
-        <p className={styles.errorMessage}>
-          {errorMessage}
-        </p>
-      )
-    }
-  }
 
   return (
     <div>
       <Head>
-        <title>Example</title>
-        <meta name="description" content="Example app" />
+        <title>LW3Punks</title>
+        <meta name="description" content="LW3Punks-Dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        { loading ? (
-          <div className={styles.loading}>
-            <p>Loading...</p>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Random Winner Game!</h1>
+          <div className={styles.description}>
+            It's a lottery game where a winner is chosen at random and wins the
+            entire lottery pool
           </div>
-        ) : ''}
-        {renderButton()}
-        {renderMessages()}
+          {renderButton()}
+          {logs &&
+            logs.map((log, index) => (
+              <div className={styles.log} key={index}>
+                {log}
+              </div>
+            ))}
+        </div>
+        <div>
+          <img className={styles.image} src="./randomWinner.png" />
+        </div>
       </div>
 
-      <footer className={styles.footer}>Created by Falcon Andrea</footer>
+      <footer className={styles.footer}>Made with &#10084; by Your Name</footer>
     </div>
-  )
+  );
 }
